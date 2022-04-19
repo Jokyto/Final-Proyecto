0a8bf18315f7865cc6f4785bb77ebc97a7689f0b
MattCGx <mattcontrerasg@gmail.com>
2022-04-19 02:29:30 -0300

    post process daño
    agregado post process daño

--- Assets/Scenes/Desafio/Scripts/Global_PP_Controler.cs
+++ Assets/Scenes/Desafio/Scripts/Global_PP_Controler.cs
@@ -6,24 +6,59 @@ public class Global_PP_Controler : MonoBehaviour
 {
     private PostProcessVolume Global_volume;
     private Vignette colorEffect;
-    private ColorGrading Dead_effect;
-    private bool IsRuntineDead = false;
-    private bool IsRuntineDamage = false;
+    //private ColorGrading Dead_effect;
+    //private bool IsRuntineDead = false;
+    //private bool IsRuntineDamage = false;
 
     private void Awake() 
     {
         Global_volume = GetComponent<PostProcessVolume>();
         Global_volume.profile.TryGetSettings(out colorEffect);
-        Global_volume = GetComponent<PostProcessVolume>();
-        Global_volume.profile.TryGetSettings(out Dead_effect);
+        //Global_volume = GetComponent<PostProcessVolume>();
+        //Global_volume.profile.TryGetSettings(out Dead_effect);
     }
     private void Start() {
         FindObjectOfType<PlayerCollision>().onGetDamage += DamageEffect;
     }
-
     private void Update() 
     {
-        if (Input.GetKeyDown(KeyCode.F2))
+      
+    }
+     /*private void Death()
+    {
+        if (!IsRuntineDead)
+        {
+            Dead_effect.active = true;
+            IsRuntineDead = true;
+        } 
+    }
+
+    private void Alive()
+    {
+        Dead_effect.active = false;
+        IsRuntineDead = false;
+    }*/
+
+    IEnumerator Damage()
+    {
+        colorEffect.active = true;
+        yield return new WaitForSeconds(0.7f);
+        colorEffect.active = false;       
+    }
+
+
+    public void DamageEffect()
+    {
+       // if (!IsRuntineDamage && !IsRuntineDead) {}
+        
+            StartCoroutine(Damage());
+        
+    }
+
+
+
+
+  /*if (Input.GetKeyDown(KeyCode.F2))
         {
             if (!IsRuntineDead)
             {
@@ -37,35 +72,5 @@ public class Global_PP_Controler : MonoBehaviour
         if (Input.GetKey(KeyCode.F1))
         {       
                 DamageEffect();      
-        }
-    }
-    private void Death()
-    {
-        if (!IsRuntineDead)
-        {
-            Dead_effect.active = true;
-            IsRuntineDead = true;
-        } 
-    }
-
-    private void Alive()
-    {
-        Dead_effect.active = false;
-        IsRuntineDead = false;
-    }
-    IEnumerator Damage()
-    {
-        colorEffect.active = true;
-        yield return new WaitForSeconds(0.7f);
-        colorEffect.active = false;       
-    }
-
-
-    public void DamageEffect()
-    {
-        if (!IsRuntineDamage && !IsRuntineDead)
-        {
-            StartCoroutine(Damage());
-        }
-    }
+        }*/
 }
